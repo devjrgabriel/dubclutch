54101ab0657db05b1a09a89e271121cae91c94f2
devjrgabriel <devjrgabrielsouza@gmail.com>
2024-05-31 21:57:54 -0300

    att

--- dubclutch/src/pages/login/index.js
+++ dubclutch/src/pages/login/index.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import GoogleImg from '../../image/google.png';
 import './login.css';
 import './login.js';
 import { MostrarLogin, MostrarRegistro, login, registrar } from './login.js';
 
-export default function Login() {
+export const Login = () => {
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
+   const navigate = useNavigate();
+
+   const iniciarevento = (event) => {
+      event.preventDefault()
+      if (email === 'a@com' && password === 'a') {
+         alert('Login bem-sucedido!')
+         navigate('/Home');
+      } else {
+         alert('Credenciais invalidas')
+
+      }
+   }
    return (
       <>
          <div className="login-container">
             <div className="login-card">
-               <form id="formularioLogin" className="input-class" onsubmit="return login()">
+               <form id="formularioLogin" className="input-class" onSubmit={iniciarevento}>
                   <h2 className="title-login">Faça o seu login</h2>
                   <label className="label-login">Email:</label>
-                  <input type="text" id="login-username" placeholder="exemplo@gmail.com" required />
+                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required placeholder="speedrun@gmail.com" className="inputForm" name="email" id="email" />
                   <label className="label-login">Senha:</label>
-                  <input type="password" id="login-password" placeholder="Digite sua senha..." required />
+                  <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required placeholder="**********" className="inputForm" name="password" id="password" />
                   <button type="submit" className="" onClick={login}>Entrar</button>
                   <button type="submit" className="bg-google"> <img className="img-input" src={GoogleImg} /> Continue com
                      Google</button>
--- dubclutch/src/router.js
+++ dubclutch/src/router.js
@@ -2,15 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from './components/Header';
 import Home from "./pages/home";
-import Login from "./pages/login";
+import {Login} from "./pages/login";
 
 export default function RouteApp() {
     return (
         <BrowserRouter>
-            <Header />
             <Routes>
-                <Route path="/" element={<Home />}/>/
-                <Route path="/Login" element={<Login />}/>
+                <Route path="/" element={<Login />}/>/
+                <Route path="/Home" element={<Home />}/>/
             </Routes>
 
         </BrowserRouter>
