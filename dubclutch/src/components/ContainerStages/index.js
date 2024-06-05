7152582dcff9f53fda7a2e5f38411e6d34d1ff55
gustavoofranca <engs-gustavofranca@camporeal.edu.br>
2024-06-04 21:21:53 -0300

    att

--- dubclutch/src/components/ContainerStages/index.js
+++ dubclutch/src/components/ContainerStages/index.js
@@ -3,15 +3,59 @@ import './stages.css';
 export const ContainerStages =() =>{
     return(
                     <>
-            <section className="section1 secao">
-                <div className="containerTextos1">
-                    <p className="title1">
-                        aaaaaaaaaaaaaaaaaaaaaaaaaaa
+            <section className="sectionStage">
+                <div className="mainDiv">
+                <div className="containerStages">
+                    <p className='stageP'>
+                        Stage, o que é?
                     </p>
-                    <p className="Subtitulo1">
+                    <p className="stageQ">
+                        Os "Stages" como são popularmente chamados, são literalmente, estágios de preparação de veículos, avançando cada vez mais conforme os niveis conforme os niveis do stage "instalado".
                     </p>
                 </div>
+
+                <div className="containerStagecard1 ">
+                <div className="cardsStagesection">
+                    <div className="textcardStageSection2">
+                        <span className="titleStagecards">Stage 1</span>
+                        <span className="subtituloStagecards">O mais básico dos stages conta somente com a parte da reprogramação do software que cuida das estrategias da ECU do carro.</span> 
+
+                    </div>
+                </div>
+
+                <div className="cardsStagesection">
+                    <div className="textcardStageSection2">
+                        <span className="titleStagecards">Stage 2</span>
+                        <span className="subtituloStagecards">O mais popular entre eles por conta do custo beneficio que oferece conta com:<br/>
+                        Remap<br/>
+                        Troca do filtro de ar por um filtro de ar esportivo (opcional)<br/>
+                        Troca do downpipe por um com polegada maior<br/>
+                        Troca das velas por velas mais frias (opcional)<br/>
+                        Troca das bobinas (opcional)<br/>
+                        Instalação de Blow-off (opcional)<br/>
+                    </span>         
+                    </div>
+                </div>
+
+                <div className="cardsStagesection">
+                    <div className="textcardStageSection2">
+                        <span className="titleStagecards">Stage 3</span>
+                        <span className="subtituloStagecards">Stage 2 - Motor 4B11T- I4</span>
+                    </div>
+                </div>
+
+                <div className="cardsStagesection">
+                    <div className="textcardStageSection2">
+                        <span className="titleStagecards">Stage 4</span>
+                        <span className="subtituloStagecards">Stage 2 - Motor 4B11T- I4</span>
+                    </div>
+                </div>
+
+
+            </div>
+            </div>
             </section>
+
         </>
         )
 }
